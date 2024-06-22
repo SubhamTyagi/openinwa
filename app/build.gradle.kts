a4e351c06725b2328110de42cc8639389c39b16f
Shubham Tyagi <shubham2tyagi7@gmail.com>
2024-06-22 21:35:34 +0530

    update version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
    // implementation(libs.androidx.material3)
-    implementation ("com.google.android.material:material:1.12.0")
+    implementation (libs.material)
     implementation(libs.appcompat)
     implementation(libs.preference)
     implementation(libs.libphonenumber)
