03c6ac47eca624dde9e9367cc7f0448344282f4b
Shubham Tyagi <shubham2tyagi7@gmail.com>
2024-07-02 16:19:42 +0530

    add workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -36,9 +36,9 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+  //  composeOptions {
+   //     kotlinCompilerExtensionVersion = "1.5.1"
+  //  }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,11 +50,11 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    //implementation(libs.androidx.activity.compose)
+  //  implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
+  //  implementation(libs.androidx.ui.graphics)
+   // implementation(libs.androidx.ui.tooling.preview)
    // implementation(libs.androidx.material3)
     implementation (libs.material)
     implementation(libs.appcompat)
@@ -65,10 +65,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
 
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+   // androidTestImplementation(libs.androidx.junit)
+   // androidTestImplementation(libs.androidx.espresso.core)
+   // androidTestImplementation(platform(libs.androidx.compose.bom))
+  //  androidTestImplementation(libs.androidx.ui.test.junit4)
+  //  debugImplementation(libs.androidx.ui.tooling)
+   // debugImplementation(libs.androidx.ui.test.manifest)
 }
